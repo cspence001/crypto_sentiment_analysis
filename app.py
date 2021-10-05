dc38236f1c5f5de93ec2a0119781b609e4b16b6d
Ciara Spencer <ciara.spencer314@gmail.com>
2021-10-04 22:46:51 -0400

    file update

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     return render_template("test2.html")
 
+
+@app.route("/analysis")
+def analysis():
+    return render_template("analysis.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
